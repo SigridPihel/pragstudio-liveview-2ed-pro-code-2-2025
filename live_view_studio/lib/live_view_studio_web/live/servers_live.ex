f57c8f6e3a02997580c4e73aa55dab71d107baa9
SigridPihel <sigrid.pihel@yolo.com>
2025-04-10 15:02:05 +0300

    Chapter 11 cideo code done

--- live_view_studio/lib/live_view_studio_web/live/servers_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/servers_live.ex
@@ -4,31 +4,54 @@ defmodule LiveViewStudioWeb.ServersLive do
   alias LiveViewStudio.Servers
 
   def mount(_params, _session, socket) do
+    IO.inspect(self(), label: "MOUNT")
     servers = Servers.list_servers()
 
     socket =
       assign(socket,
         servers: servers,
-        selected_server: hd(servers),
         coffees: 0
       )
 
     {:ok, socket}
   end
 
+  def handle_params(%{"id" => id}, _uri, socket) do
+    IO.inspect(self(), label: "HANDLE PARAMS ID=#{id}?")
+
+    server = Servers.get_server!(id)
+
+    {:noreply,
+      assign(socket,
+        selected_server: server,
+        page_title: "What's up #{server.name}"
+      )}
+  end
+
+  def handle_params(_, _uri, socket) do
+    IO.inspect(self(), label: "HANDLE PARAMS CATCH-ALL")
+
+    {:noreply,
+    assign(socket,
+      selected_server: hd(socket.assigns.servers)
+    )}
+  end
+
   def render(assigns) do
+    IO.inspect(self(), label: "RENDER")
     ~H"""
     <h1>Servers</h1>
     <div id="servers">
       <div class="sidebar">
         <div class="nav">
-          <a
+          <.link
             :for={server <- @servers}
+            patch={~p"/servers?#{[id: server]}"}
             class={if server == @selected_server, do: "selected"}
           >
             <span class={server.status}></span>
             <%= server.name %>
-          </a>
+          </.link>
         </div>
         <div class="coffees">
           <button phx-click="drink">
@@ -64,7 +87,11 @@ defmodule LiveViewStudioWeb.ServersLive do
               </blockquote>
             </div>
           </div>
-          <div class="links"></div>
+          <div class="links">
+            <.link navigate={~p"/light"}>
+              Adjust lights
+            </.link>
+          </div>
         </div>
       </div>
     </div>
@@ -72,6 +99,8 @@ defmodule LiveViewStudioWeb.ServersLive do
   end
 
   def handle_event("drink", _, socket) do
+    IO.inspect(self(), label: "HANDLE DRINK EVENT")
+
     {:noreply, update(socket, :coffees, &(&1 + 1))}
   end
 end
