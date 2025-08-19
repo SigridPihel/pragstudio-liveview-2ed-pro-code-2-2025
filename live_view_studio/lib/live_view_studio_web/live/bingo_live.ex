ab4545358b22e8810e8d38efc84803cbbdeeeee9
SigridPihel <sigrid.pihel@yolo.com>
2025-08-19 10:18:34 +0300

    Exercise: Track Presence for Bingo Players done

--- live_view_studio/lib/live_view_studio_web/live/bingo_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/bingo_live.ex
@@ -1,17 +1,32 @@
 defmodule LiveViewStudioWeb.BingoLive do
   use LiveViewStudioWeb, :live_view
 
+  alias LiveViewStudioWeb.Presence
+
+  @topic "users:bingo"
+
   def mount(_params, _session, socket) do
+    %{current_user: current_user} = socket.assigns
+
     if connected?(socket) do
+      Presence.subscribe(@topic)
       # send message
+
+      {:ok, _} =
+        Presence.track_user(current_user, @topic, %{
+          timestamp: Timex.now() |> Timex.format!("%H:%M", :strftime)
+        })
+
       :timer.send_interval(3000, self(), :tick)
     end
 
+    presences = Presence.list_users(@topic)
+
     socket =
-      assign(socket,
-        number: nil,
-        numbers: all_numbers()
-      )
+      socket
+      |> assign(:number, nil)
+      |> assign(:numbers, all_numbers())
+      |> assign(:presences, Presence.simple_presence_map(presences))
 
     {:ok, socket}
   end
@@ -20,10 +35,28 @@ defmodule LiveViewStudioWeb.BingoLive do
     ~H"""
     <h1>Bingo Boss 📢</h1>
     <div id="bingo">
+      <div class="users">
+        <ul>
+          <.presence :for={{_user_id, meta} <- @presences} meta={meta} />
+        </ul>
+      </div>
       <div class="number">
         {@number}
       </div>
     </div>
+    """
+  end
+
+  def presence(assigns) do
+    ~H"""
+    <li>
+      <span class="username">
+        <%= @meta.username %>
+      </span>
+      <span class="timestamp">
+        <%= @meta.timestamp %>
+      </span>
+    </li>
     """
   end
 
@@ -54,5 +87,9 @@ defmodule LiveViewStudioWeb.BingoLive do
       Enum.map(numbers, &"#{letter}  #{&1}")
     end)
     |> Enum.shuffle()
+  end
+
+  def handle_info(%{event: "presence_diff", payload: diff}, socket) do
+    {:noreply, Presence.handle_diff(socket, diff)}
   end
 end
